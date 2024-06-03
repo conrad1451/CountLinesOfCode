81232dd7f91a5de98d4dfc6bea6d7cb491753997
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-03 01:00:24 -0400

    Fixing bugs

--- src/App2.tsx
+++ src/App2.tsx
@@ -2,9 +2,8 @@
 
 // Importing modules
 // import React, { useState, useEffect } from "react";
-import React from "react";
-import useState from "react";
-
+ import React, { useState } from 'react';
+ 
 import "./App.css";
 
 function App() {
