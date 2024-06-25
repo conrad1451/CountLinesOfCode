90346c32fcc64aa3ababcecf09a914157877f85f
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-25 11:43:22 -0400

    Create constants.tsx

---
+++ src/app/utils/constants.tsx
@@ -0,0 +1,2 @@
+export const THE_TEXT = `https://raw.githubusercontent.com/conrad1451/CountLinesOfCode/main/src/sampleText.txt`
+export const API_BASE_URL = `https://anime-backend-z9eb.onrender.com/api/`
