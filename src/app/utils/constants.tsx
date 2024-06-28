f11c3dd41eacf8c4aee88c9d02a921f8b21a81ea
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-28 14:57:35 -0400

    Create constants.tsx

---
+++ src/app/utils/constants.tsx
@@ -0,0 +1,9 @@
+export const THE_TEXT = `https://raw.githubusercontent.com/conrad1451/CountLinesOfCode/main/src/sampleText.txt`
+export const OLD_API_BASE_URL = `https://anime-backend-z9eb.onrender.com/api/`
+
+
+export const FLASK_API_URL = `https://flask-simple-urjk.onrender.com/`
+export const NODE_API_URL = `https://github-repos-nodejs-api-json.onrender.com/`
+
+export const FLASK_API_URL_ONLY_ENDPOINT = FLASK_API_URL
+export const NODE_API_URL_ONLY_ENDPOINT = NODE_API_URL
